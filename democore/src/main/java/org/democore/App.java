6021b204d403785c193ebbf98e882f1d7187ecd9
niteshyadav1671995 <ny1671995@gmail.com>
2018-04-26 10:54:22 +0530

    first commit

---
+++ democore/src/main/java/org/democore/App.java
@@ -0,0 +1,13 @@
+package org.democore;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
